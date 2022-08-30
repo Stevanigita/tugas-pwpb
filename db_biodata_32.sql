4d6719c31648e2902b7300d5c34688d1383bacce
stevani <stevanigita73265@gmail.com>
2022-08-30 14:42:50 +0700

    tugas

---
+++ db_biodata_32.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 30, 2022 at 09:29 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_biodata_32`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `db_biodata_32`
+--
+
+CREATE TABLE `db_biodata_32` (
+  `id_bio` int(11) NOT NULL,
+  `NIK` varchar(16) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jk` enum('laki-laki','perempuan') NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `no hp` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
